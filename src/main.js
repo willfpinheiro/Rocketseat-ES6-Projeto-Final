de54fd1d2a827288dbad276a5c625381d92435f9
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-12-04 10:23:17 -0300

    feat finalizado configuracao webpack

---
+++ src/main.js
@@ -0,0 +1,13 @@
+class App {
+	constructor() {
+		this.repositories = [];
+
+		this.formEl = document.getElementById('repo-form');
+
+		this.registerHandlers();
+	}
+
+	registerHandlers() {}
+
+	addRepository() {}
+}
