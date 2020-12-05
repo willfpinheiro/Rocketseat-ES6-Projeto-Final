4204635b9b406ed80ffd84b235f2e901354d9548
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-12-05 12:03:28 -0300

    feat render em tela

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ class App {
 		this.repositories = [];
 		// saber pelo js o que esta acontecendo no form
 		this.formEl = document.getElementById('repo-form');
+		this.listEl = document.getElementById('repo-list');
 
 		this.registerHandlers();
 	}
@@ -22,7 +23,34 @@ class App {
 			avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
 			html_url: 'http://github.com/rocketseat/rocketseat.com.br',
 		});
-		console.log(this.repositories);
+		this.render();
+	}
+
+	render() {
+		this.listEl.innerHTML = '';
+
+		this.repositories.forEach((repo) => {
+			let imgEl = document.createElement('img');
+			imgEl.setAttribute('src', repo.avatar_url);
+
+			let titleEl = document.createElement('strong');
+			titleEl.appendChild(document.createTextNode(repo.name));
+
+			let descriptionEl = document.createElement('p');
+			descriptionEl.appendChild(document.createTextNode(repo.description));
+
+			let linkEl = document.createElement('a');
+			linkEl.setAttribute('target', '_brank');
+			linkEl.appendChild(document.createTextNode('Acessar'));
+
+			let listItemEl = document.createElement('li');
+			listItemEl.appendChild(imgEl);
+			listItemEl.appendChild(titleEl);
+			listItemEl.appendChild(descriptionEl);
+			listItemEl.appendChild(linkEl);
+
+			this.listEl.appendChild(listItemEl);
+		});
 	}
 }
 
