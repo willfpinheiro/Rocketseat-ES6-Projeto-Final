020f51cdcd82c921bb9ddf0413948a9f0cfa790d
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-12-05 11:38:32 -0300

    Feat adicionando repositorios

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
 class App {
 	constructor() {
+		// armazenar as repos adicionadas
 		this.repositories = [];
-
+		// saber pelo js o que esta acontecendo no form
 		this.formEl = document.getElementById('repo-form');
 
 		this.registerHandlers();
 	}
 
-	registerHandlers() {}
+	registerHandlers() {
+		this.formEl.onsubmit = (event) => this.addRepository(event);
+	}
 
-	addRepository() {}
+	addRepository(event) {
+		// nao deixa o forma recarregar a pagina, de fazer o post
+		event.preventDefault();
+
+		this.repositories.push({
+			name: 'rocketseat.com.br',
+			description: 'Tire a sua ideia do papel',
+			avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+			html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+		});
+		console.log(this.repositories);
+	}
 }
+
+// const MeuApp = new App();
+new App();
