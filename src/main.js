0874e199c3e220873cd42937b8d5d5c3229287fd
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-12-05 13:00:27 -0300

    feat buscando da api

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import api from './api';
+
 class App {
 	constructor() {
 		// armazenar as repos adicionadas
 		this.repositories = [];
 		// saber pelo js o que esta acontecendo no form
 		this.formEl = document.getElementById('repo-form');
+		this.inputEl = document.querySelector('input[name=repository');
 		this.listEl = document.getElementById('repo-list');
 
 		this.registerHandlers();
@@ -13,16 +16,35 @@ class App {
 		this.formEl.onsubmit = (event) => this.addRepository(event);
 	}
 
-	addRepository(event) {
+	async addRepository(event) {
 		// nao deixa o forma recarregar a pagina, de fazer o post
 		event.preventDefault();
 
+		const repoInput = this.inputEl.value;
+
+		if (repoInput.length === 0) {
+			// colocando so um return, faz com que a requisição seja encerrada nesse ponto
+			return;
+		}
+
+		const response = await api.get(`/repos/${repoInput}`);
+
+		const {
+			name,
+			description,
+			html_url,
+			owner: { avatar_url },
+		} = response.data;
+
 		this.repositories.push({
-			name: 'rocketseat.com.br',
-			description: 'Tire a sua ideia do papel',
-			avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-			html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+			name,
+			description,
+			html_url,
+			avatar_url,
 		});
+
+		this.inputEl.value = '';
+
 		this.render();
 	}
 
@@ -41,6 +63,7 @@ class App {
 
 			let linkEl = document.createElement('a');
 			linkEl.setAttribute('target', '_brank');
+			linkEl.setAttribute('href', repo.html_url);
 			linkEl.appendChild(document.createTextNode('Acessar'));
 
 			let listItemEl = document.createElement('li');
